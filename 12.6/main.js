470961b33f5f23c302ff0002ecbe7b1fe95d82aa
Kevin <thehailang@gmail.com>
2017-03-05 13:50:29 +0800

    lesson point

---
+++ 12.6/main.js
@@ -0,0 +1,19 @@
+var day = 4;
+switch(day) {
+  case 1:
+  // ...
+  // ...
+  console.log('今天吃牛肉面');
+  break;
+  case 2:
+  // ...
+  console.log('今天吃羊肉泡');
+  break;
+  case 3:
+  // ...
+  console.log('今天吃干锅千叶豆腐');
+  break;
+  default:
+  console.log('今天吃臊子面');
+  break;
+}
